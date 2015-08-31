02c5754a9e770a8821e1bb65c2a49e8f3f17693f
Baka9k <neoplanspaceliner9@gmail.com>
2015-09-01 00:05:20 +0300

    2 new methods

--- heap.js
+++ heap.js
@@ -1,17 +1,17 @@
-/**
-* 1)Создать кучу                   -    <имя кучи> = new Heap()
-* 2)Добавить элемент в кучу        -    <имя кучи>.insert(<объект, имеющий свойство "p", означающее его приоритет>)
-* 3)Взять из кучи элемент 
-*  с наибольшим приоритетом        -    <имя кучи>.extract()
-* 4)Посмотреть элемент 
-*  с наибольшим приоритетом        -    <имя кучи>.peekMin()      
-* 5)Вернуть кучу в виде массива,
-*  в котором все объекты
-*  отсортированы по прио-
-*  ритету (с высоким в 
-*  начале массива)                 -    <>.getSortedArray()
+п»ї/**
+* 1)РЎРѕР·РґР°С‚СЊ РєСѓС‡Сѓ                   -    <РёРјСЏ РєСѓС‡Рё> = new Heap()
+* 2)Р”РѕР±Р°РІРёС‚СЊ СЌР»РµРјРµРЅС‚ РІ РєСѓС‡Сѓ        -    <РёРјСЏ РєСѓС‡Рё>.insert(<РѕР±СЉРµРєС‚, РёРјРµСЋС‰РёР№ СЃРІРѕР№СЃС‚РІРѕ "p", РѕР·РЅР°С‡Р°СЋС‰РµРµ РµРіРѕ РїСЂРёРѕСЂРёС‚РµС‚>)
+* 3)Р’Р·СЏС‚СЊ РёР· РєСѓС‡Рё СЌР»РµРјРµРЅС‚ 
+*  СЃ РЅР°РёР±РѕР»СЊС€РёРј РїСЂРёРѕСЂРёС‚РµС‚РѕРј        -    <РёРјСЏ РєСѓС‡Рё>.extract()
+* 4)РџРѕСЃРјРѕС‚СЂРµС‚СЊ СЌР»РµРјРµРЅС‚ 
+*  СЃ РЅР°РёР±РѕР»СЊС€РёРј РїСЂРёРѕСЂРёС‚РµС‚РѕРј        -    <РёРјСЏ РєСѓС‡Рё>.peekMin()      
+* 5)Р’РµСЂРЅСѓС‚СЊ РєСѓС‡Сѓ РІ РІРёРґРµ РјР°СЃСЃРёРІР°,
+*  РІ РєРѕС‚РѕСЂРѕРј РІСЃРµ РѕР±СЉРµРєС‚С‹
+*  РѕС‚СЃРѕСЂС‚РёСЂРѕРІР°РЅС‹ РїРѕ РїСЂРёРѕ-
+*  СЂРёС‚РµС‚Сѓ (СЃ РІС‹СЃРѕРєРёРј РІ 
+*  РЅР°С‡Р°Р»Рµ РјР°СЃСЃРёРІР°)                 -    <>.getSortedArray()
 *
-* p - приоритет, чем меньше значение p, тем выше приоритет. Если A.p = 2, B.p = 21, то A приоритетнее B.
+* p - РїСЂРёРѕСЂРёС‚РµС‚, С‡РµРј РјРµРЅСЊС€Рµ Р·РЅР°С‡РµРЅРёРµ p, С‚РµРј РІС‹С€Рµ РїСЂРёРѕСЂРёС‚РµС‚. Р•СЃР»Рё A.p = 2, B.p = 21, С‚Рѕ A РїСЂРёРѕСЂРёС‚РµС‚РЅРµРµ B.
 */
 
 function Heap() {
@@ -21,37 +21,40 @@ function Heap() {
 Heap.prototype.div = function(x,y) {
 	return (x-x%y)/y;
 }
-Array.prototype.swap = function(x,y) {
-	var t = this[x];
-	this[x] = this[y];
-	this[y] = t;
+Heap.prototype.random = function (min, max) {
+  return Math.random() * (max - min) + min;
 }
-Heap.prototype.siftUp = function(i) {
-	while ((i > 0)&&(this.arr[this.div(i,2)].p > this.arr[i].p)) {
-		this.arr.swap(i,this.div(i,2));
+Heap.prototype.swap = function(array,x,y) {
+	var t = array[x];
+	array[x] = array[y];
+	array[y] = t;
+}
+Heap.prototype.siftUp = function(arr,i) {
+	while ((i > 0)&&(arr[this.div(i,2)].p > arr[i].p)) {
+		this.swap(arr,i,this.div(i,2));
 		i = this.div(i,2);
 	}
 }
 Heap.prototype.insert = function(i) {
 	this.arr.push(i);
-	this.siftUp(this.arr.length - 1);
+	this.siftUp(this.arr,this.arr.length - 1);
 }
 Heap.prototype.peekMin = function() {
 	return this.arr[0];
 }
-Heap.prototype.siftDown = function(i) {
-	while (2*i < this.arr.length) {
+Heap.prototype.siftDown = function(arr,i) {
+	while (2*i < arr.length) {
 		var j = i;
-		if (this.arr[2*i].p < this.arr[j].p) {
+		if (arr[2*i].p < arr[j].p) {
 			j = 2*i;
 		}
-		if ((2*i + 1 <= this.arr.length)&&(this.arr[2*i + 1].p < this.arr[j].p)) {
+		if ((2*i + 1 <= arr.length)&&(arr[2*i + 1].p < arr[j].p)) {
 			j = 2*i + 1;
 		}
 		if (j == i) {
 			break;
 		}
-		this.arr.swap(i,j);
+		this.swap(arr,i,j);
 		i = j;
 	}
 }
@@ -59,16 +62,40 @@ Heap.prototype.extract = function() {
 	ext = this.arr[0];
 	this.arr[0] = this.arr[this.arr.length - 1];
 	this.arr.pop();
-	this.siftDown(0);
+	this.siftDown(this.arr,0);
 }
 Heap.prototype.bubblesort = function (arr) {
 	for (var i = 0; i < arr.length - 1; i++) {
 		for (var j = 0; j < arr.length - i - 1; j++) {
 			if (arr[j].p < arr[j + 1].p) {
-				arr.swap(j, j + 1);
+				this.swap(arr,j,j + 1);
 			}
 	    }
 	}
+}
+Heap.prototype.quicksort = function (arr) {
+	var sort = function (l,r) {
+		var x = this.random(l,r);
+		for (var i = r; i < l; i++) {
+			if (arr[i] > x) {
+				for (var j = r; j >= l; j--) {
+					if (arr[j] < x) break;
+				}
+				this.swap(arr,i,j);
+			}
+		}
+		if (r - l > 2) {
+			sort(l,r)
+		}
+	}
+}
+Heap.prototype.arr2heap = function(arr) {
+	for(var i = arr.length; i >= 0; i--) {
+		this.swap(arr,0,i)
+		console.log(i);
+		this.siftUp(arr,i);
+	}
+	
 }
 Heap.prototype.getSortedArray = function() {
 	var newArray = this.arr.slice();
