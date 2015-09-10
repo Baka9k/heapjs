79b9386221f21879fc52074b62660717bb263c80
Baka9k <neoplanspaceliner9@gmail.com>
2015-09-10 23:52:06 +0300

    Fixes

--- heap.js
+++ heap.js
@@ -9,7 +9,7 @@
 *  в котором все объекты
 *  отсортированы по прио-
 *  ритету (с высоким в 
-*  начале массива)                 -    <>.getSortedArray()
+*  начале массива)                 -    <имя кучи>.getSortedArray()
 *
 * p - приоритет, чем меньше значение p, тем выше приоритет. Если A.p = 2, B.p = 21, то A приоритетнее B.
 */
@@ -42,27 +42,28 @@ Heap.prototype.insert = function(i) {
 Heap.prototype.peekMin = function() {
 	return this.arr[0];
 }
-Heap.prototype.siftDown = function(arr,i) {
-	while (2*i < arr.length) {
+Heap.prototype.siftDown = function(i) {
+	while (2*i < this.arr.length) {
 		var j = i;
-		if (arr[2*i].p < arr[j].p) {
+		if (this.arr[2*i].p < this.arr[j].p) {
 			j = 2*i;
 		}
-		if ((2*i + 1 <= arr.length)&&(arr[2*i + 1].p < arr[j].p)) {
+		if ((2*i + 1 <= this.arr.length)&&(this.arr[2*i + 1].p < this.arr[j].p)) {
 			j = 2*i + 1;
 		}
 		if (j == i) {
 			break;
 		}
-		this.swap(arr,i,j);
+		this.swap(this.arr,i,j);
 		i = j;
 	}
 }
 Heap.prototype.extract = function() {
-	ext = this.arr[0];
+	var ext = this.arr[0];
 	this.arr[0] = this.arr[this.arr.length - 1];
 	this.arr.pop();
-	this.siftDown(this.arr,0);
+	this.siftDown(0);
+	return ext;
 }
 Heap.prototype.bubblesort = function (arr) {
 	for (var i = 0; i < arr.length - 1; i++) {
@@ -72,30 +73,6 @@ Heap.prototype.bubblesort = function (arr) {
 			}
 	    }
 	}
-}
-Heap.prototype.quicksort = function (arr) {
-	var sort = function (l,r) {
-		var x = this.random(l,r);
-		for (var i = r; i < l; i++) {
-			if (arr[i] > x) {
-				for (var j = r; j >= l; j--) {
-					if (arr[j] < x) break;
-				}
-				this.swap(arr,i,j);
-			}
-		}
-		if (r - l > 2) {
-			sort(l,r)
-		}
-	}
-}
-Heap.prototype.arr2heap = function(arr) {
-	for(var i = arr.length; i >= 0; i--) {
-		this.swap(arr,0,i)
-		console.log(i);
-		this.siftUp(arr,i);
-	}
-	
 }
 Heap.prototype.getSortedArray = function() {
 	var newArray = this.arr.slice();
